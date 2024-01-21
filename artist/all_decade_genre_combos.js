2d4de0f792c90468f8f6dde945eb9075cacf7578
Jim Kelleher <jim_kelleher@comcast.net>
2024-01-21 11:55:25 -0500

    Update all_decade_genre_combos.js
    fix file names

--- artist/all_decade_genre_combos.js
+++ artist/all_decade_genre_combos.js
@@ -9,37 +9,37 @@
       "id":"Adele",
       "decade":2010,
       "genre":"POP",
-      "image":"https://i.ytimg.com/vi/sql3dKRJy_E/maxresdefault.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Adele.jpg"
    },
    {
       "id":"Barry White",
       "decade":1970,
       "genre":"SOUL",
-      "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT-PN1Dy6i1a0smUHn6yvLeoXkvdlFyfxa7yg8KBS9bjFgXxG5Ug"
+      "image":"https://www.workingweb.info/database/images/artist/Barry White.jpg"
    },
    {
       "id":"Bob Marley",
       "decade":1970,
       "genre":"REGGAE",
-      "image":"https://www.workingweb.info/database/images/artist/BobMarley.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Bob Marley.jpg"
    },
    {
       "id":"Bobby Darin",
       "decade":1960,
       "genre":"CLASSIC",
-      "image":"https://s-media-cache-ak0.pinimg.com/736x/5b/53/a9/5b53a979c96c16579be243b1b93186f1.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Bobby Darin.jpg"
    },
    {
       "id":"Boz Scaggs",
       "decade":1970,
       "genre":"ROCK",
-      "image":"https://www.workingweb.info/database/images/artist/BozScaggs.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Boz Scaggs.jpg"
    },
    {
       "id":"Buddy Holly",
       "decade":1950,
       "genre":"ROCK",
-      "image":"https://s-media-cache-ak0.pinimg.com/originals/44/2b/b2/442bb2b6394d39d96a2d1e16061e260e.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Buddy Holly.jpg"
    },
    {
       "id":"Coldplay",
@@ -51,7 +51,7 @@
       "id":"Collective Soul",
       "decade":1990,
       "genre":"SOUL",
-      "image":"https://i.pinimg.com/736x/28/64/02/2864024d0742e61369c941fdbcc2e7b1--music-songs-songs-album.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Collective Soul.jpg"
    },
    {
       "id":"Delerium",
@@ -63,13 +63,13 @@
       "id":"Frank Sinatra",
       "decade":1940,
       "genre":"CLASSIC",
-      "image":"https://www.workingweb.info/database/images/artist/FrankSinatra.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Frank Sinatra.jpg"
    },
    {
       "id":"Henry Mancini",
       "decade":1950,
       "genre":"CLASSIC",
-      "image":"https://www.lasplash.com/uploads//1/bigband.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Henry Mancini.jpg"
    },
    {
       "id":"Jem",
@@ -81,49 +81,49 @@
       "id":"Jimmy Webb",
       "decade":1970,
       "genre":"CLASSIC",
-      "image":"https://images-na.ssl-images-amazon.com/images/I/51gTP8IWWKL.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Jimmy Webb.jpg"
    },
    {
       "id":"Johnny Cash",
       "decade":1950,
       "genre":"COUNTRY",
-      "image":"https://cp91279.biography.com/Johnny-Cash_The-Man-in-Black_HD_768x432-16x9.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Johnny Cash.jpg"
    },
    {
       "id":"Kelly Rowland",
       "decade":2000,
       "genre":"SOUL",
-      "image":"https://www.bellanaija.com/wp-content/uploads/2016/05/GettyImages-533631890.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Kelly Rowland.jpg"
    },
    {
       "id":"Kris Kristofferson",
       "decade":1970,
       "genre":"COUNTRY",
-      "image":"https://www.workingweb.info/database/images/artist/KrisKristofferson.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Kris Kristofferson.jpg"
    },
    {
       "id":"Little Richard",
       "decade":1950,
       "genre":"SOUL",
-      "image":"https://www.workingweb.info/database/images/artist/LittleRichard.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Little Richard.jpg"
    },
    {
       "id":"Lyle Lovett",
       "decade":1980,
       "genre":"COUNTRY",
-      "image":"https://www.workingweb.info/database/images/artist/LyleLovett.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Lyle Lovett.jpg"
    },
    {
       "id":"No Doubt",
       "decade":1990,
       "genre":"ROCK",
-      "image":"https://www.workingweb.info/database/images/artist/NoDoubt.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/No Doubt.jpg"
    },
    {
       "id":"Sarah Brightman",
       "decade":1990,
       "genre":"CLASSIC",
-      "image":"https://images1.buymusichere.net/images/universal/162/04228391162.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Sarah Brightman.jpg"
    },
    {
       "id":"Seal",
@@ -135,54 +135,54 @@
       "id":"Sly and the Family Stone",
       "decade":1960,
       "genre":"SOUL",
-      "image":"https://www.workingweb.info/database/images/artist/SlyAndTheFamilyStone.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Sly And The Family Stone.jpg"
    },
    {
       "id":"The B-52's",
       "decade":1980,
       "genre":"POP",
-      "image":"https://s-media-cache-ak0.pinimg.com/236x/96/f5/a4/96f5a4d895d946dadfc5e88a9d0f8f53.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/The B-52's.jpg"
    },
    {
       "id":"The Cardigans",
       "decade":1990,
       "genre":"CHILL",
-      "image":"https://www.workingweb.info/database/images/artist/TheCardigans.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/The Cardigans.jpg"
    },
    {
       "id":"The Pretenders",
       "decade":1980,
       "genre":"ROCK",
-      "image":"https://d1ya1fm0bicxg1.cloudfront.net/2015/04/promoted-media-optimized_55341fc64d20d.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/The Pretenders.jpg"
    },
    {
       "id":"The Who",
       "decade":1960,
       "genre":"ROCK",
-      "image":"https://www.workingweb.info/database/images/artist/TheWho.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/The Who.jpg"
    },
    {
       "id":"Tom Jones",
       "decade":1960,
       "genre":"POP",
-      "image":"https://www.workingweb.info/database/images/artist/TomJones.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Tom Jones.jpg"
    },
    {
       "id":"Tycho",
       "decade":2010,
       "genre":"CHILL",
-      "image":"https://poskod.my/wp-content/uploads/2015/01/Tycho2.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Tycho.jpg"
    },
    {
       "id":"Zero 7",
       "decade":2000,
       "genre":"CHILL",
-      "image":"https://www.rightchordmusic.co.uk/wp/wp-content/uploads/2015/08/dear-plastic-001.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Zero 7.jpg"
    },
    {
       "id":"Ziggy Marley",
       "decade":2000,
       "genre":"REGGAE",
-      "image":"https://www.workingweb.info/database/images/artist/ZiggyMarley.jpg"
+      "image":"https://www.workingweb.info/database/images/artist/Ziggy Marley.jpg"
    }
 ]
